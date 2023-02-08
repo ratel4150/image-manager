936f413211fcb705a12f85da3302faa07238dbb5
ratel4150 <rratel76@gmail.com>
2023-02-08 23:05:13 +0100

    se crea formulario de carga de imagenes

---
+++ front-end-image-manager-app/src/componentes/FormularioDeCargaDeImagenes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FormularioDeCargaDeImagenes() {
+  return (
+    <div className="formulario-carga-imagenes-app">FormularioDeCargaDeImagenes</div>
+  )
+}
+
+export default FormularioDeCargaDeImagenes
